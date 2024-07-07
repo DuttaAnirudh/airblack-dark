dec8a7139300e336689c7ee8add1e8d3a1ea0a03
DuttaAnirudh <worktodutta@gmail.com>
2024-07-07 22:24:51 +0530

    fix: fix the image carousel infinite slide effect

--- src/ui/ImageCarousel.jsx
+++ src/ui/ImageCarousel.jsx
@@ -1,47 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 const ImageCarousel = ({ images }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(1); // Start from the first real image
-  const [isTransitioning, setIsTransitioning] = useState(true);
-
-  const handleTransitionEnd = () => {
-    if (currentImageIndex === images.length + 1) {
-      setIsTransitioning(false);
-      setCurrentImageIndex(1); // Jump back to the first real image
-    } else if (currentImageIndex === 0) {
-      setIsTransitioning(false);
-      setCurrentImageIndex(images.length); // Jump back to the last real image
-    }
-  };
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const extendedImages = useRef(images);
 
   useEffect(() => {
-    if (!isTransitioning) {
-      setTimeout(() => {
-        setIsTransitioning(true);
-      }, 50); // Re-enable transition after the jump
-    }
-  }, [isTransitioning]);
+    const cloneArray = () => {
+      extendedImages.current = [...extendedImages.current, ...images];
+    };
 
+    const intervalId = setInterval(cloneArray, 6000);
+
+    // Clean up the interval on component unmount
+    return () => clearInterval(intervalId);
+  }, [images]);
+
+  // Effect to handle automatic sliding
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => prevIndex + 1);
-    }, 2000); // Change image every 1 seconds
+    }, 3000); // Change image every 3 seconds
 
     return () => clearInterval(interval);
   }, []);
 
-  const extendedImages = [images[images.length - 1], ...images, images[0]];
-
   return (
     <div className="relative w-full h-full overflow-hidden">
       <div
-        className={`flex transition-transform duration-1000 ease-in-out h-full ${
-          !isTransitioning ? "transition-none" : ""
-        }`}
+        className={`flex transition-transform duration-1000 ease-in-out h-full`}
         style={{ transform: `translateX(-${currentImageIndex * 100}%)` }}
-        onTransitionEnd={handleTransitionEnd}
       >
-        {extendedImages.map((image, index) => (
+        {extendedImages.current.map((image, index) => (
           <div
             key={index}
             className={`w-full flex-shrink-0 h-full bg-cover bg-center`}
